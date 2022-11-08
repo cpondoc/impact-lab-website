3cb6eabdb966ac4c772e92e8dbc6c29115139ea2
cpondoc <chris.pondoc@comcast.net>
2022-11-07 23:17:13 -0800

    updated to gatsby

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,176 +1,136 @@
+// Necessary React Dependencies
 import * as React from "react"
+import Accordion from 'react-bootstrap/Accordion';
 
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
+// Styling
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../style/index.css';
 
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
+// Lab Images
+import Icon from '../images/icon.png'
+import KingCenter from '../images/king-center.png'
+import PovertyCenter from '../images/poverty-center.jpeg'
+import NaturalCapital from '../images/natural-capital.png'
+import LawSchool from '../images/law-school.png'
 
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
-
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
+// Team Images
+import Chris from '../images/pondoc.jpeg'
+import Phil from '../images/phil.png'
 
 const IndexPage = () => {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>
-        Congratulations
+    <div className="container">
+      <img id="logo" src={Icon} alt="CS+SG Logo"/>
+      <h1>Impact Lab
         <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
+        <h3>Helping promote equity and access to undergraduate research</h3>
       </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time. 😎
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
-    </main>
+      <p>The Impact Lab, created by CS+Social Good, aims to build a community of undergraduate researchers conducting socially-impactful research. We seek to provide students with faculty mentorship, community, and relevant resources that enable effective social impact-related technical research projects. Our central initiative is the Research Exposure Program, which directs committed undergraduates to social-impact-focused research groups. The CS+Social Good Impact Lab is supported by the Stanford Institute for Human-Centered AI.</p>
+      <br />
+      <div className="row">
+        <div className="col-sm">
+          <img className="icons" src={KingCenter} alt="King Center"/>
+        </div>
+        <div className="col-sm">
+          <img className="icons" src={PovertyCenter} alt="Poverty Center"/>
+        </div>
+        <div className="col-sm">
+          <img className="icons" src={NaturalCapital} alt="Natural Capital Project"/>
+        </div>
+        <div className="col-sm">
+          <img className="icons" src={LawSchool} alt="Stanford Law School"/>
+        </div>
+      </div>
+      <br />
+      <h3 className="header">How to Apply
+        <br />
+        <p>Below are some answers to frequently asked questions about applying to Impact Lab.</p>
+      </h3>
+      <Accordion>
+        <Accordion.Item eventKey="0">
+          <Accordion.Header><p className="accordion-header">What does the Impact Lab Application process look like?</p></Accordion.Header>
+          <Accordion.Body>
+            <ul>
+              <li>Fill out our Google Form application by December 3rd</li>
+              <li>Wait for lab matches, which will be emailed over winter break.</li>
+              <li>If accepted by a lab, get excited and welcome to Impact Lab!</li>
+              <li>Once you receive your match make sure to reach out to your lab.</li>
+              <li>Get ready for weekly Impact Lab meetings starting winter quarter.</li>
+            </ul>
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="1">
+          <Accordion.Header><p className="accordion-header">What does the research process look like?</p></Accordion.Header>
+          <Accordion.Body>
+            <ul>
+                <li>The research process varies from lab to lab, but students will typically get started by going through a training program specific to their role.</li>
+                <li>From there, students may be asked to engage in a variety of tasks, including data analysis, needs finding, and software/application development.</li>
+            </ul>
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="2">
+          <Accordion.Header><p className="accordion-header">What level of experience do I need?</p></Accordion.Header>
+          <Accordion.Body>
+            <ul>
+                <li>No prior research/technical experience is necessary!</li>
+                <li>We expect Impact Lab to be students’ first research experience, so don’t feel discouraged to apply if you haven’t taken part in research before.</li>
+            </ul>
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="3">
+          <Accordion.Header><p className="accordion-header">Will I get compensated for my work?</p></Accordion.Header>
+          <Accordion.Body>
+            <ul>
+                <li>Students can receive academic credit for all projects, but some labs do offer paid positions as well.</li>
+            </ul>
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="4">
+          <Accordion.Header><p className="accordion-header">What kind of support does Impact Lab give?</p></Accordion.Header>
+          <Accordion.Body>
+            <ul>
+                <li>Impact Lab meets every week to support the research cohort! We do activities such as: research skills workshops, speaker panels, movie/TV screenings, and other fun CS/research related things!</li>
+            </ul>
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
+      <br />
+      <h3 className="header">Timeline
+        <br />
+        <p>Below are some key dates:</p>
+        <p><b><u>November 14:</u></b> Applications Open</p>
+        <p><b><u>December 14:</u></b> Applications Close</p>
+        <p><b><u>Winter Break:</u></b> Decisions Released</p>
+      </h3>
+      <br />
+      <h3 className="header">Team
+        <br />
+        <p>Here's the team helping run Impact Lab this year!</p>
+      </h3>
+      <div className="row">
+        <div className="col-sm">
+          <img className="icons" src={Chris} alt="Chris Pondoc"/>
+          <p>Chris Pondoc</p>
+        </div>
+        <div className="col-sm">
+          <img className="icons" src={Phil} alt="Phil Bailargeron"/>
+          <p>Phil Bailargeron</p>
+        </div>
+        <div className="col-sm">
+        </div>
+        <div className="col-sm">
+        </div>
+      </div>
+      <br />
+      <h3 className="header">Contact
+        <br />
+        <p>Have any questions? Reach out to Impact Lab Co-Leads Christopher Pondoc (clpondoc@stanford.edu) or Phil Bailargeron (pabaill@stanford.edu) if you have any questions!</p>
+      </h3>
+      <br />
+    </div>
   )
 }
 
 export default IndexPage
 
-export const Head = () => <title>Home Page</title>
+export const Head = () => <title>Home | Impact Lab</title>
